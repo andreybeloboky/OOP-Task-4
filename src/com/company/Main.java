e67b9306283e0c64e67288c9e49ed9ee5f5c379e
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-10-19 18:03:09 +0300

    add array name and three object/class: Manager, SalesMan, Decorator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String[] name = {"Andrew", "Bober", "Sergey", "Inga", "Ina", "Inna", "Olya", "Nella", "Anton", "Kirill"};
+        int begin = 1;
+        int end = 10;
+        int randomName = (int) (Math.random() * end);
+        Manager[] manager = new Manager[begin + (int) (Math.random() * end)];
+        SalesMan[] salesMan = new SalesMan[begin + (int) (Math.random() * end)];
+        Decorator[] decorator = new Decorator[begin + (int) (Math.random() * end)];
     }
 }
